de5f6dcb08ccf7719c78eeee0b1706a0c8b98bb4
Justin Worley <jkworley@gmail.com>
2023-05-01 10:57:44 -0400

    commit tree 08b2bc08f1e1c9c3bcc477df70f6ffa3176476db

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let distanceFromHqInBlocks = Math.abs(someValue - 42);
+    return distanceFromHqInBlocks;
+}
+
+function distanceFromHqInFeet(someValue) {
+    let distanceFromHqInFeet = distanceFromHqInBlocks(someValue) * 264;
+    return distanceFromHqInFeet;
+}
